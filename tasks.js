a9ccdcb9f306568762d9c9560ab2d4c18e580704
lvchsk <dienislev@mail.ru>
2024-12-23 20:24:32 +0300

    tasksFixes2

--- tasks.js
+++ tasks.js
@@ -1,7 +1,9 @@
 window.addEventListener("load", async () => {
   const tg = window.Telegram.WebApp;
   tg.expand();
-console.log(tg);
+
+  alert(`Платформа: ${tg.platform}`);
+  
 
   const container = document.getElementById("container");
   container.style.overflow = "auto";
@@ -35,8 +37,6 @@ console.log(tg);
     "query_id=AAFnEKlRAAAAAGcQqVFuPynM&user=%7B%22id%22%3A1370034279%2C%22first_name%22%3A%22PUG%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22mad_pug%22%2C%22language_code%22%3A%22ru%22%2C%22is_premium%22%3Atrue%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2FC4P153sbF5ZKQ0bXj61fro_kpL2AtsdVnAeOQ2veP_Y.svg%22%7D&auth_date=1734346396&signature=tkzPFjA-_u5sC8Jn29G8iy3729vIL8fyHHyALY4aVmsnqtNXcCalZzS4GmnglECiEcO1SdHCgFeJDUHyFiPiCg&hash";
 
   async function fetchTasks() {
-    // console.log(platf);
-
     try {
       const response = await fetch("https://bestx.cam/tasks", {
         method: "POST",
@@ -48,25 +48,12 @@ console.log(tg);
       if (!response.ok) {
         throw new Error(`Ошибка: ${response.status}`);
       }
-
-      // Преобразуем ответ в JSON
       const tasks = await response.json();
-      console.log(tasks);
-
-      // Вызываем функцию для отображения задач
       displayTasks(tasks.projects);
     } catch (error) {
       console.error("Ошибка при получении задач:", error);
     }
   }
-
-  //   const tasks = [
-  //     { name: "Задание 1", id: 1 },
-  //     { name: "Задание 2", id: 2 },
-  //     { name: "Задание 3", id: 3 },
-  //     { name: "Задание 4", id: 4 },
-  //   ];
-  //   console.log(tasks);
 
   function displayTasks(tasks) {
     const taskList = document.getElementById("taskList");
@@ -92,7 +79,6 @@ console.log(tg);
   }
 
   fetchTasks();
-  //   displayTasks(tasks);
 
   function highlightActiveLink() {
     const currentPath = window.location.pathname;
