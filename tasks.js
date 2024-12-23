b455298cd5169e8a0ed6edb3b260251dee2ce137
lvchsk <dienislev@mail.ru>
2024-12-23 20:11:25 +0300

    tasksFixes

--- tasks.js
+++ tasks.js
@@ -1,37 +1,42 @@
 window.addEventListener("load", async () => {
   const tg = window.Telegram.WebApp;
   tg.expand();
+console.log(tg);
 
-  const container = document.getElementById('container');
-  container.style.overflow = 'auto';
-  container.style.height = '80vh';
-  container.style.position = 'absolute';
+  const container = document.getElementById("container");
+  container.style.overflow = "auto";
+  container.style.height = "80vh";
+  container.style.position = "absolute";
 
-  const userDeviceArray = [
-    { device: "ios", platform: /iPhone|iPad|Macintosh/ }, // Все Apple устройства
-    { device: "android", platform: /Android|Symbian|Windows Phone|Tablet OS|Linux|Windows NT/ } // Все остальные
-  ];
-  
-  const platf = navigator.userAgent;
-  
-  function getPlatform() {
-    for (let i in userDeviceArray) {
-      if (userDeviceArray[i].platform.test(platf)) {
-        return userDeviceArray[i].device;
-      }
-    }
-    return "Неизвестная платформа!";
-  }
-  
-  console.log("Ваша платформа: " + getPlatform()); 
-  const platform = getPlatform() 
+  // const userDeviceArray = [
+  //   { device: "ios", platform: /iPhone|iPad|Macintosh/ }, // Все Apple устройства
+  //   { device: "android", platform: /Android|Symbian|Windows Phone|Tablet OS|Linux|Windows NT/ } // Все остальные
+  // ];
+
+  // const platf = navigator.userAgent;
+
+  // function getPlatform() {
+  //   for (let i in userDeviceArray) {
+  //     if (userDeviceArray[i].platform.test(platf)) {
+  //       return userDeviceArray[i].device;
+  //     }
+  //   }
+  //   return "Неизвестная платформа!";
+  // }
+
+  // console.log("Ваша платформа: " + getPlatform());
+  // const platform = getPlatform()
+
+  const platform = Telegram.WebApp.platform;
+
+  console.log(`Платформа: ${platform}`);
 
   const initData =
     "query_id=AAFnEKlRAAAAAGcQqVFuPynM&user=%7B%22id%22%3A1370034279%2C%22first_name%22%3A%22PUG%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22mad_pug%22%2C%22language_code%22%3A%22ru%22%2C%22is_premium%22%3Atrue%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2FC4P153sbF5ZKQ0bXj61fro_kpL2AtsdVnAeOQ2veP_Y.svg%22%7D&auth_date=1734346396&signature=tkzPFjA-_u5sC8Jn29G8iy3729vIL8fyHHyALY4aVmsnqtNXcCalZzS4GmnglECiEcO1SdHCgFeJDUHyFiPiCg&hash";
 
   async function fetchTasks() {
-    console.log(platf);
-    
+    // console.log(platf);
+
     try {
       const response = await fetch("https://bestx.cam/tasks", {
         method: "POST",
@@ -47,7 +52,6 @@ window.addEventListener("load", async () => {
       // Преобразуем ответ в JSON
       const tasks = await response.json();
       console.log(tasks);
-      
 
       // Вызываем функцию для отображения задач
       displayTasks(tasks.projects);
@@ -62,7 +66,7 @@ window.addEventListener("load", async () => {
   //     { name: "Задание 3", id: 3 },
   //     { name: "Задание 4", id: 4 },
   //   ];
-//   console.log(tasks);
+  //   console.log(tasks);
 
   function displayTasks(tasks) {
     const taskList = document.getElementById("taskList");
